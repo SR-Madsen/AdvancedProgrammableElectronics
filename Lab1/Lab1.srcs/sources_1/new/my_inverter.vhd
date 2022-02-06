6959d219fb7c61cda92a58a02f512a1bf23bcb11
SR-Madsen <ligthflash@gmail.com>
2022-02-06 12:10:46 +0100

    Added attempt at integrating inverters in VHDL, non-functional.

--- Lab1/Lab1.srcs/sources_1/new/my_inverter.vhd
+++ Lab1/Lab1.srcs/sources_1/new/my_inverter.vhd
@@ -2,36 +2,51 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-
---entity my_inverter is
---    Port ( A_I : in STD_LOGIC;
---           Q_O : out STD_LOGIC);
---end my_inverter;
-
 entity my_inverter is
-        Port (CLK_I : in STD_LOGIC;
-              Q_O : out STD_LOGIC_VECTOR(6 downto 0));
+        Port (CLK_O : out STD_LOGIC;
+              LED_O : out STD_LOGIC_VECTOR(6 downto 0);
+              U10_IO : inout STD_LOGIC;
+              U13_I : in STD_LOGIC);
 end my_inverter;
+
 
 architecture Behavioral of my_inverter is
 
---begin
---    Q_O <= not A_I;
---end Behavioral;
 
-signal q : std_logic_vector(6 downto 0) := "0000000";
+signal leds : std_logic_vector(6 downto 0) := "0000000";
 signal clk : std_logic;
 
+signal q13 : std_logic;
+signal q12_11 : std_logic;
+signal q10 : std_logic;
+signal q09 : std_logic;
+
+
 begin
-    clk<=CLK_I;
-    Q_O<=q;
+    -- Input to first inverter
+    q13 <= U13_I;
     
+    -- Instantly propagate through another inverter
+    q12_11 <= not q13;
+    q10 <= not q12_11;
+
+    -- Output to capacitor.
+    U10_IO <= q10;
+    
+    -- Read back from capacitor, output as clock through inverter
+    q09    <= U10_IO;
+    clk    <= not q09;
+
+    -- Connect clock and LED outputs
+    CLK_O <= clk;
+    LED_O <= leds;
+    
+    -- LED update
     process(clk) begin
         if rising_edge(clk) then
-            q<=q+1;
+            leds <= leds + 1;
         end if;
     end process;
-    
 end Behavioral;
 
 
