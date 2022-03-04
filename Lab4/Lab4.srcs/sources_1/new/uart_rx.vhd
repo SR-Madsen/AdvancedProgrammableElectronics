9d9ac95ac35c6b89028f3a1e1c42dc4f762c0b51
SR-Madsen <ligthflash@gmail.com>
2022-03-04 13:18:49 +0100

    Made changes to UART RX, simulated but untested.

--- Lab4/Lab4.srcs/sources_1/new/uart_rx.vhd
+++ Lab4/Lab4.srcs/sources_1/new/uart_rx.vhd
@@ -12,10 +12,10 @@ end uart_rx;
 
 architecture Behavioral of uart_rx is
 
-signal baud_counter : integer range 0 to 15 := 0;
+signal baud_counter : integer range 0 to 15 := 8;
 
-type STATE_TYPE is (Stop, Start, Bit0, Bit1, Bit2, Bit3, Bit4, Bit5, Bit6, Bit7);
-signal state : STATE_TYPE := Stop;
+type STATE_TYPE is (Idle, Stop, Start, Bit0, Bit1, Bit2, Bit3, Bit4, Bit5, Bit6, Bit7);
+signal state : STATE_TYPE := Idle;
 
 
 begin
@@ -25,28 +25,33 @@ begin
     begin
         if RESET = '1' then
             baud_counter <= 0;
-        elsif CLK'event and CLK = '1' then            
+        elsif CLK'event and CLK = '1' and state /= Idle then            
             if baud_counter < 15 then
                 baud_counter <= baud_counter + 1;
             else
                 baud_counter <= 0;
             end if;
         end if;
+        if state = Idle then
+            baud_counter <= 8;
+        end if;
     end process;
     
     -- Handshake signal
-    READY <= '1' when state = Stop else '0';
+    READY <= '1' when state = Idle else '0';
     
     -- Next state logic
     Next_state: process(CLK, RESET)
     begin
         if RESET = '1' then
-            state <= Stop;
+            state <= Idle;
         elsif CLK'event and CLK = '1' then
+            if state = Idle and RX = '0' then
+                state <= Start;
+            end if;
             if baud_counter = 0 then
                 case state is
-                    when Stop  => if RX = '0' then
-                                  state <= Start; end if;
+                    when Idle  => null;
                     when Start => state <= Bit0;
                     when Bit0  => state <= Bit1;
                     when Bit1  => state <= Bit2;
@@ -56,6 +61,7 @@ begin
                     When Bit5  => state <= Bit6;
                     When Bit6  => state <= Bit7;
                     When Bit7  => state <= Stop;
+                    when Stop  => state <= Idle;
                 end case;
             end if;
         end if;
@@ -65,14 +71,14 @@ begin
     Output_logic: process(state)
     begin
         case state is
-            when Bit0   =>  DATA(0) <= RX;
-            when Bit1   =>  DATA(1) <= RX;
-            when Bit2   =>  DATA(2) <= RX;
-            when Bit3   =>  DATA(3) <= RX;
-            when Bit4   =>  DATA(4) <= RX;
-            when Bit5   =>  DATA(5) <= RX;
-            when Bit6   =>  DATA(6) <= RX;
-            when Bit7   =>  DATA(7) <= RX;
+            when Bit1   =>  DATA(0) <= RX;
+            when Bit2   =>  DATA(1) <= RX;
+            when Bit3   =>  DATA(2) <= RX;
+            when Bit4   =>  DATA(3) <= RX;
+            when Bit5   =>  DATA(4) <= RX;
+            when Bit6   =>  DATA(5) <= RX;
+            when Bit7   =>  DATA(6) <= RX;
+            when Stop   =>  DATA(7) <= RX;
             when Start  =>  DATA    <= x"00";
             when others =>  null;
         end case;
